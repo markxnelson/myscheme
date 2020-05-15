6242d5f87313b181eecfe01a83e0c890ac45aa76
Mark Nelson <mark.x.nelson@oracle.com>
2020-05-14 22:26:42 -0400

    exercise 1

--- app/Main.hs
+++ app/Main.hs
@@ -4,4 +4,4 @@ import System.Environment
 main :: IO ()
 main = do
     args <- getArgs
-    putStrLn  ("Hello, " ++ head args)
+    putStrLn  ("Hello, " ++ head args ++ ". I hope you are " ++ args !! 1)
